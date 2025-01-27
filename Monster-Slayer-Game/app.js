d236b77d1855ef754f43e697237403a9420ed946
Athef Ak <athefak@Athefs-MacBook-Pro.local>
2025-01-27 15:02:10 -0500

    Using Watchers to add Game Over

--- Monster-Slayer-Game/app.js
+++ Monster-Slayer-Game/app.js
@@ -10,7 +10,7 @@ const app = Vue.createApp({
             playerHealth: 100,
             monsterHealth: 100,
             currentRound : 0,
-
+            winner: null
         }
     },
     computed: {
@@ -22,6 +22,26 @@ const app = Vue.createApp({
         },
         mayUseSpecialAttack(){
             return this.currentRound % 3 !==0
+        }
+    },
+    watch:{
+        playerHealth(value){
+            if(value <= 0 && this.monsterHealth <= 0){
+                // A Draw
+                this.winner = 'draw'
+            } else if (value <= 0){
+                //Monster Won
+                this.winner = 'monster'
+            }
+        },
+        monsterHealth(value){
+            if(value <= 0 && this.playerHealth <= 0){
+                // A Draw
+                this.winner = 'draw'
+            } else if (value <= 0){
+                //Player Won
+                this.winner = 'player'
+            }
         }
     },
     methods: {
