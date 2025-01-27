de12b98b2101450260cef13840c084a6e29fa879
Athef Ak <athefak@Athefs-MacBook-Pro.local>
2025-01-27 14:49:08 -0500

    Add heal Player Functionality

--- Monster-Slayer-Game/app.js
+++ Monster-Slayer-Game/app.js
@@ -9,7 +9,8 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
-            currentRound : 0
+            currentRound : 0,
+
         }
     },
     computed: {
@@ -38,6 +39,16 @@ const app = Vue.createApp({
             this.currentRound++;
             const attackValue = getRandomValue(10,25)
             this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        healPlayer(){
+            this.currentRound++;
+            const healValue = getRandomValue(8, 20);
+            if(this.playerHealth + healValue > 100){
+                this.playerHealth = 100;
+            } else{
+                this.playerHealth += healValue;
+            }
             this.attackPlayer();
         }
     }
