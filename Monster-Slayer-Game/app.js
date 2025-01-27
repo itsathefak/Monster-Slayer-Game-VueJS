7f77b1640a6783ebf8993e1cc3a1b963f5522da4
Athef Ak <athefak@Athefs-MacBook-Pro.local>
2025-01-27 14:36:27 -0500

    Update Health Bar

--- Monster-Slayer-Game/app.js
+++ Monster-Slayer-Game/app.js
@@ -1,25 +1,33 @@
-function getRandomValue (min, max){
-    Math.floor( Math.random() * (max-min)) + min; //Calculate the Min and Max damage made by player and monster
+function getRandomValue(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min; // Add +1 to include max value
 }
 
 const app = Vue.createApp({
-    data(){
-        return{
+    data() {
+        return {
             playerHealth: 100,
             monsterHealth: 100
         }
     },
-    methods:{
-        attackMonster(){ //Calculate the damage for the moster
-        const attackValue = getRandomValue(5, 12) //Add that Min and Max val
-        this.mosterHealth -= attackValue;
-        this.attackPlayer();
+    computed: {
+        monsterBarStyles() {
+            return { width: this.monsterHealth + '%' }
+        },
+        playerBarStyles() {
+            return { width: this.playerHealth + '%' }
+        }
     },
-        attackPlayer(){ //Calculate the damage for the player
-        const attackValue = getRandomValue(8, 15) //Add that Min and Max val
-        this.playerHealth -= attackValue;
+    methods: {
+        attackMonster() {
+            const attackValue = getRandomValue(5, 12); // Min-Max damage for the monster
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        attackPlayer() {
+            const attackValue = getRandomValue(8, 15); // Min-Max damage for the player
+            this.playerHealth -= attackValue;
         }
     }
-})
+});
 
-app.mount("#game")
+app.mount("#game");
