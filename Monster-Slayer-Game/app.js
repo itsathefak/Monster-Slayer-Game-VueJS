c0b7784870ad4e577ec86d4da4f1dbc8cde049ad
Athef Ak <athefak@Athefs-MacBook-Pro.local>
2025-01-27 15:06:58 -0500

    Add Start New Game Function

--- Monster-Slayer-Game/app.js
+++ Monster-Slayer-Game/app.js
@@ -15,9 +15,15 @@ const app = Vue.createApp({
     },
     computed: {
         monsterBarStyles() {
+            if(this.monsterHealth < 0){
+                return { width: '0%'}
+            }
             return { width: this.monsterHealth + '%' }
         },
         playerBarStyles() {
+            if(this.playerHealth < 0){
+                return { width: '0%'}
+            }
             return { width: this.playerHealth + '%' }
         },
         mayUseSpecialAttack(){
@@ -45,6 +51,12 @@ const app = Vue.createApp({
         }
     },
     methods: {
+        startGame(){
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.winner = null;
+            this.currentRound = 0;
+        },
         attackMonster() {
             this.currentRound++;
             const attackValue = getRandomValue(5, 12); // Min-Max damage for the monster
