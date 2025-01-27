c5b3a317b1a27da0b9677e05494f669860d67d14
Athef Ak <athefak@Athefs-MacBook-Pro.local>
2025-01-27 14:43:56 -0500

    Create Special Attack Functionality

--- Monster-Slayer-Game/app.js
+++ Monster-Slayer-Game/app.js
@@ -2,11 +2,14 @@ function getRandomValue(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min; // Add +1 to include max value
 }
 
+
+
 const app = Vue.createApp({
     data() {
         return {
             playerHealth: 100,
-            monsterHealth: 100
+            monsterHealth: 100,
+            currentRound : 0
         }
     },
     computed: {
@@ -15,10 +18,14 @@ const app = Vue.createApp({
         },
         playerBarStyles() {
             return { width: this.playerHealth + '%' }
+        },
+        mayUseSpecialAttack(){
+            return this.currentRound % 3 !==0
         }
     },
     methods: {
         attackMonster() {
+            this.currentRound++;
             const attackValue = getRandomValue(5, 12); // Min-Max damage for the monster
             this.monsterHealth -= attackValue;
             this.attackPlayer();
@@ -26,6 +33,12 @@ const app = Vue.createApp({
         attackPlayer() {
             const attackValue = getRandomValue(8, 15); // Min-Max damage for the player
             this.playerHealth -= attackValue;
+        },
+        specialAttackMonster(){
+            this.currentRound++;
+            const attackValue = getRandomValue(10,25)
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
         }
     }
 });
